26ab88c0adf2a40d76c6d611266385517d0e3695
andrew <aloesel2@gmail.com>
2022-03-29 10:04:10 -0400

    update work

--- src/main/scala/firstScala.scala
+++ src/main/scala/firstScala.scala
@@ -1,19 +1,18 @@
 import scala.annotation.tailrec
-import scala.collection.immutable.Nil.:::
 
 object ex extends App {
   // checking for member
   // [[HW1: (10 pts) complete the member function
   def member(a: Int, b: List[Int]): Boolean = {
-    val size = b.length
-    var contains = false
-    for (i <- 0 to  size - 1){
-      if (b(i) == a){
-        contains = true
+    if (b != Nil){
+      if (b.head == a){
+        true
+      }else{
+        member(a, b.tail)
       }
+    }else{
+      false
     }
-    //return
-    contains
   }
 
   val l4 = List(1, 2, 3, 4)
@@ -27,20 +26,16 @@ object ex extends App {
   val l5 = List('A', 'C', 'D')
 
   def equalsimp(a: List[Char], b: List[Char]): Boolean = {
-
- var eq = true
-    var s1 = a.length
-    var s2 = b.length
-    if (s1 != s2) {
-      eq = false
-      return eq
+    //see if the inputs are null
+    if(a == Nil  && b == Nil){
+      true
+    }else{
+      if (a.head == b.head){
+        equalsimp(a.tail, b.tail)
+      }else{
+        false
+      }
     }
-for(i <- 0 to s1){
-  if(a(i) != b(i)){
-    eq = false
-  }
-}
-    eq
   }
 
   println(equalsimp(l1, l5))
@@ -51,7 +46,7 @@ for(i <- 0 to s1){
   // [[HW3: (10 pts) Complete the append function
   def append[A](l1: List[A], l2: List[A]): List[A] = {
     l1 ::: l2
-    }
+  }
   println(append(l1, l6))
   println(append(l5, l1))
 
@@ -111,64 +106,65 @@ for(i <- 0 to s1){
   // HW6]]
 
   // [[HW7: (10 pts) Complete the function composition
-  def h[A, B, C](f: B => C, g: A => B, x : A): C ={
-    f(g(x))
+  def h[A, B, C](f: B => C, g: A => B): A => C ={
+    f compose g
   }
 
 
   val add2 = (x: Int) => x + 2
   val times3 = (x: Int) => x * 3
-  println(h(add2, times3, 4))
+  println(h(add2, times3)(4))
 
   //HW7]
 
   // [[HW8: (10 pts) Complete the third_list function
   // it should return the third element of the list
   def third_list[A](ls: List[A]): A ={
-    ls(3)
+    //we do list (2) which gives us the third element since lists start at 0 index
+    ls(2)
   }
 
 
-  val l1 = List(1, 2, 3, 4)
-  println(third_list(l1))
+  val l11 = List(1, 2, 3, 4)
+  println(third_list(l11))
   println(third_list(List(1,2,(3,4,5), 6, 7)))
   // HW8]]
 
   // [[HW9: (10 pts) Complete the nth_list function
   // this function returns the nth element in the list
   def nth_list[A](n: Int, ls: List[A]): A = {
-    ls(n)
+    //to get the the nth element in a list we call the index of n -1 since index is 0 based
+    ls(n - 1)
   }
-  println(nth_list(2, l1))
+  println(nth_list(2, l11))
   println(nth_list(3, List(1,2,(3,4,5), 6, 7)))
   // HW9]]
 
   // [[HW10: (10 pts) Complete the following function
+  //todo do this one
   def applyall[A, B](fun:A => B, l:List[A]): List[B] ={
-    var newList: List[B] = new List[B]
-    for(i <- 0 to length(l)){
-      newList = newList ::: fun(l(i))
-    }
-    newList
+    var lol: List[B] = List()
+    lol
   }
+
 
 
   def cube(x: Int): Int = x * x * x
 
-  val l1 = List(2,3,4)
-  println(applyall(cube, l1))
+  val l12 = List(2,3,4)
+  println(applyall(cube, l12))
   println(applyall((x: Int) => x * x * x, List(3, 4, 5)))
   // HW10]]
   // [[HW11: (10 pts) Find the length of a list recursively
   def length[A](l: List[A]): Int = {
-    if(l != null) {
+    if(l != Nil) {
       1 + length(l.tail)
     }else
-      1
+      0
 
   }
-    println(length(l1))
+
+
+    println(length(l12))
   println(length(List(1,2,(3,4,5), 6, 7)))
-  }
-
-
+}
